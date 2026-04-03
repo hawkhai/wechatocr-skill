03ff6829e145123c42228429c60571440829463b
yqh <haihoing@qq.com>
2026-04-04 00:37:47 +0800

    remove batfiles

--- upgrade_launcher.bat
+++
@@ -1,9 +0,0 @@
-cd /d "%~dp0"
-
-IF NOT EXIST "wechatocr\wechatocr.exe" (
-    tar -xf wechatocr.zip
-)
-
-call upgrade.bat
-
-call wechatocr.bat
--- wechatocr.bat
+++
@@ -1,8 +0,0 @@
-cd /d "%~dp0"
-
-cd wechatocr\serv\runtime
-start infserv64.exe
-timeout /t 1 >nul
-
-cd ..\..\..
-wechatocr\wechatocr.exe %*
